3ee128cc25e520d248dc237a3f7b9a2cdfd5ac3f
JackZhou303 <jackzhou302@gmail.com>
2019-12-15 05:33:30 +0800

    Updated Dashboard auth block

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -18,28 +18,31 @@ class UserProfile extends Component {
     return current_user
   }
 
-  componentDidMount(){
+  async componentDidMount(){
     const user= this.get_current_user();
-    this.setState({
-      currentUser: user
-    }, console.log(user))
     //init create first user instance
-    let user_snapshot;
-    firebase.database.ref(user.uid).once("value").then(function(snapshot){
-      user_snapshot=snapshot.val();
-      if(!user_snapshot || !user_snapshot.hasProfile){
-        firebase.database.ref(user.uid).set({
-            displayName: user.displayName,
-            email: user.email,
-            username: " ", 
-            scores: 0,
-            played_games: 0,
-            hasProfile: true
-        })
-      }
-    })
+    let user_snapshot, user_info;
+    user_snapshot= await firebase.database.ref(user.uid).once("value");
+    user_snapshot= user_snapshot.val()
 
-    
+    if(!user_snapshot || !user_snapshot.hasProfile){
+      firebase.database.ref(user.uid).set({
+          displayName: user.displayName,
+          email: user.email,
+          username: " ", 
+          scores: 0,
+          played_games: 0,
+          hasProfile: true
+      })
+      user_snapshot= await firebase.database.ref(user.uid).once("value");
+      user_info= user_snapshot.val()
+
+      console.log(user_info)
+    } else user_info= user_snapshot;
+
+    this.setState({
+      currentUser: user_info
+    }, console.log(user_info))
   }
 
 // auth handler to block un auth user
@@ -49,7 +52,7 @@ class UserProfile extends Component {
           <Jumbotron className="background-transparent">
           <h1>Welcome to the Dashboard</h1>
             <p>Display Name: {this.state.currentUser.displayName}</p>
-            <p>Username: {this.state.currentUser.username}</p>
+            <p>Scores: {-1* this.state.currentUser.scores}</p>
           </Jumbotron>
           </Container>
 
